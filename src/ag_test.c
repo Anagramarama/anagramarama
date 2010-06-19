71781896ed840cc46015dcefa1ad9834231b2b07
Pat Thoyts <patthoyts@users.sourceforge.net>
2010-06-19 09:35:02 +0100

    Checked the tests using valgrind and eliminated misused calls.
    Signed-off-by: Pat Thoyts <patthoyts@users.sourceforge.net>

--- src/ag_test.c
+++ src/ag_test.c
@@ -1,5 +1,8 @@
+#ifndef UNIT_TEST
 #define UNIT_TEST
+#endif
 #include "unittest.h"
+
 #define main ag_main
 #include "ag.c"
 #undef main
@@ -17,8 +20,12 @@ static int test_nextBlank()
 static int test_shiftLeftKill()
 {
     char a[7] = { 'a','b','c','d','e','f', 0 };
-    test_equals_str("shiftLeftKill", "bcdef", shiftLeftKill(a));
-    test_equals_str("shiftLeftKill const str", "bcdef", shiftLeftKill("abcdef"));
+    char *s = shiftLeftKill(a);
+    test_equals_str("shiftLeftKill", "bcdef", s);
+    free(s);
+    s = shiftLeftKill("abcdef");
+    test_equals_str("shiftLeftKill const str", "bcdef", s);
+    free(s);
     return 0;
 }
 
@@ -31,26 +38,6 @@ static int test_shiftLeft()
     return 0;
 }
 
-static int test_swapChars()
-{
-    char a[7] = { 'a','b','c','d','e','f', 0 };
-    const char *p = a, *q = NULL;
-    test_equals_str("swapChars end", "fbcdea", swapChars(0, 5, a));
-    q = swapChars(2, 3, a);
-    test_equals_str("swapChars inner", "fbdcea", q);
-    test_equals_ptr("swapChars ptr equiv", p, q);
-    return 0;
-}
-
-static int test_revFirstNonSpace()
-{
-    char a[7] = { 'a','b','c','d','e','f', 0 };
-    char b[7] = { 'a','b',SPACE_CHAR,'d','e',SPACE_CHAR, 0 };
-    test_equals_int("rev no space", 6, revFirstNonSpace(a));
-    test_equals_int("rev find space", 5, revFirstNonSpace(b));
-    return 0;
-}
- 
 static int test_whereinstr()
 {
     test_equals_int("where is b", 1, whereinstr("abcdef",'b'));
@@ -63,18 +50,12 @@ struct unit_test_t unit_tests[] = {
     {NULL, test_shiftLeftKill, NULL},
     {NULL, test_shiftLeft, NULL},
     {NULL, test_nextBlank, NULL},
-    {NULL, test_swapChars, NULL},
-    {NULL, test_revFirstNonSpace, NULL},
     {NULL, test_whereinstr, NULL},
 };
 
 int
 main(void)
 {
-    size_t n;
-    for (n = 0; n < sizeof(unit_tests)/sizeof(unit_tests[0]); ++n) {
-        run_tests(&unit_tests[n]);
-    }
-    test_print_results();
+    run_tests(unit_tests);
     return 0;
 }
